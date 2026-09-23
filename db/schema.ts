c16e50887d48b3405570097ff39a426d179b2573
Sonu N Mahto <sonu.mahto362000@gmail.com>
2026-09-23 22:50:17 +0530

    sidebar

--- db/schema.ts
+++ db/schema.ts
@@ -1,3 +1,5 @@
+import { boolean, uuid } from "drizzle-orm/gel-core";
+import { varchar } from "drizzle-orm/mysql-core";
 import {integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
@@ -8,6 +10,27 @@ export const users = pgTable("users", {
   usageCredits: integer('usageCredits').default(100),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
+
+export const tools = pgTable("tools", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  slug: varchar("slug", { length: 100 }).notNull().unique(),
+  name: varchar("name", { length: 150 }).notNull(),
+  description: text("description"),
+
+  category: varchar("category", { length: 100 }).notNull(),
+  type: varchar("type", { length: 50 }).notNull(),
+  provider: varchar("provider", { length: 100 }).notNull(),
+
+  icon: varchar("icon", { length: 100 }),
+
+  status: varchar("status", { length: 50 }).default("active"),
+
+  requiresAuth: boolean("requires_auth").default(false),
+
+  authType: varchar("auth_type", {length: 50})
+});
+
 
 
 export type User = typeof users.$inferSelect;
